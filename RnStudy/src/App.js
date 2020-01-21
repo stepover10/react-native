818f37748530a8012ef8d1aced173cb49432aab8
stepover10 <stepover10@cashwalk.io>
2020-01-21 18:21:25 +0900

    no message

---
+++ RnStudy/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+// Redux Setting
+const App = () => {
+  return (    
+    <View>
+      <Text>
+        React Native Setting New!
+      </Text>      
+    </View>
+  )
+}
+
+export default App;
